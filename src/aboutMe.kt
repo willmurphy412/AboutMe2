4e9b27b2b89398cb423afee9e6d2ce5495e6d21a
willmurphy412 <wmurphy@students.mchenry.edu>
2022-01-19 14:04:30 -0600

    Adding extra comment Now adding Additional println statements

--- src/aboutMe.kt
+++ src/aboutMe.kt
@@ -1,5 +1,7 @@
 fun main(){
     //this is me making changes
     println("Hi! My name is Will")
+    println("I am a full time worker as a NOC technician")
     println("I enjoy Astronomy and Computers")
+    println("I have also built my own computer mostly for leisure use")
 }
